aa5cead7df4a728c58de41b7c039fffc095ead49
SeongJeong <jsj046@naver.com>
2024-01-23 21:17:07 +0900

    [Refactor] BJ1912.java

--- BJ/BJ1912.java
+++ BJ/BJ1912.java
@@ -14,16 +14,20 @@ public class BJ1912 {
         for (int i = 0; i < n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         int sum = array[0];
         int result = array[0];
         for (int i = 1; i < n; i++) {
+            // 누적 합이 양수인 경우 계속 더함
             if (sum > 0) {
                 sum += array[i];
-            } else {
+            } 
+            // 누적 합이 음수인 경우 합 갱신
+            else {
                 sum = array[i];
             }
-
+            
+            // 합과 현재 최대값 비교 후 더 큰 값 저장
             result = Math.max(sum, result);
         }
         System.out.println(result);
