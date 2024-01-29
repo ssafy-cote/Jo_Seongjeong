9f2acde9baaef90cb89e4bc68b5434903fc9b8df
SeongJeong <jsj046@naver.com>
2024-01-29 20:36:55 +0900

    [Add] BJ2839.java

---
+++ BJ/BJ2839.java
@@ -0,0 +1,27 @@
+package Jo_Seongjeong.BJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ2839 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sugar = Integer.parseInt(br.readLine());
+        int min = sugar;
+
+        for(int i = 0; i*5 <= sugar; i++) {
+            int count = 0;
+            int temp = sugar - (5*i);
+
+            if(temp % 3 == 0) {
+                count = i;
+                count += temp/3;
+                min = Math.min(min, count);
+            }
+        }
+        if(min == sugar) {
+            min = -1;
+        }
+        System.out.println(min);
+    }
+}
